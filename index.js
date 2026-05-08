97a4795ff1e8ea2888c5ac779481ebda34813a18
shervinabishek <shervinabishek@gmail.com>
2026-05-08 11:39:00 +0530

    implemented index.js first time

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import busRoute from "./routes/busRoute.js";
+import bookingRoute from "./routes/bookingRoute.js";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+        console.log("Database connected successfully");
+        app.listen(PORT, () => {
+            console.log(`Server is running on ${PORT}`);
+        });
+    }).catch((error) => console.log(error));
+
+app.use('/api/buses', busRoute);
+app.use('/api/bookings', bookingRoute);
